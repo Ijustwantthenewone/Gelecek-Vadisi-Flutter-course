0c6e41d0dc64bda6ee229ccd6ff61d80a7b80b2e
Ijustwantthenewone <ahmetridvanordulu@gmail.com>
2024-07-02 17:36:06 +0300

    sd
    sd

---
+++ path_provier/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Selam());
+}//14:30 https://drive.google.com/file/d/1FBV-FWG88rJEz2U2bxiyLgNsojGG1yTx/view
+
+class Selam extends StatelessWidget {
+  const Selam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
